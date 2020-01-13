72302d0e6f32d8abca3ef73f6c35649d2606d30e
Axel Rivera <axel@axelrivera.me>
2020-01-12 20:49:11 -0500

    Modify s3 client

--- app/lib/symbols_service.rb
+++ app/lib/symbols_service.rb
@@ -2,7 +2,7 @@ class SymbolsService
   SYMBOLS_FILE = "symbols.json".freeze
 
   def signed_symbols_url
-    signer = Aws::S3::Presigner.new
+    signer = Aws::S3::Presigner.new(client: s3_client)
     signer.presigned_url(:get_object, bucket: symbols_bucket, key: SYMBOLS_FILE)
   end
 
@@ -10,15 +10,28 @@ class SymbolsService
     payload = iex.get_symbols_string
     raise Exception.new "symbols payload empty" if payload.blank?
 
-    resource = Aws::S3::Resource.new
+    resource = Aws::S3::Resource.new(region: region)
     object = resource.bucket(symbols_bucket).object(SYMBOLS_FILE)
     object.put(body: payload, content_type: 'application/json')
+    Rails.logger.info "uploaded #{SYMBOLS_FILE} to bucket #{symbols_bucket}"
   end
 
   private
 
+  def region
+    ENV['AWS_REGION']
+  end
+
   def symbols_bucket
     ENV['AWS_S3_SYMBOLS_BUCKET']
+  end
+
+  def s3_client
+    @s3_client ||= Aws::S3::Client.new(
+      region: region,
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
   end
 
   def iex
